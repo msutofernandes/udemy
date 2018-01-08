a53c1b8fe94e566991b91f260b7649a3521af8f4
Marcus Fernandes <msutofernandes@gmail.com>
2018-01-08 10:48:34 -0500

    project created

---
+++ chess-console/chess-console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace chessconsole
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
